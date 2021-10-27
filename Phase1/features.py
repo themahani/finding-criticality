ea62977ff66275bc3ca67b93f7e00cccf0c8a51e
Ali Mahani <ali.a.mahani@zoho.com>
2021-10-27 13:04:43 +0330

    add feature.py file

---
+++ Phase1/features.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+Here we write modules that analyze the basketed data
+and returns integers or floats to form a dataframe
+"""
+
+import numpy as np
+
+def derivative(x: np.ndarray, y: np.ndarray) -> float:
+    pass
+
+def second_derivative(x: np.ndarray, y: np.ndarray) -> float:
+    pass
+
+def curvature():
+    pass
+
+def bump():
+    pass
+
+
+def test():
+    """ main body to test your code """
+    pass
+
+
+if __name__ == "__main__":
+    test()
